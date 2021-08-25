1c74506fa9f750be43ddac565fd35ca0d568c80d
sashakever <sashakever@gmail.com>
2021-08-25 14:47:39 +0300

    commit header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,11 +1,44 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { filterCompanyByName } from '../../actions/';
+import withCompanyService from "../hoc";
+import { fetchCompaniesFromGitHub } from '../../actions';
 
 import './header.scss';
 
-const Header = () => {
+const Header = ({ searchText, onFilterCompany, onFathCompanies }) => {
+    //debugger
     return (
-        <div>Header</div>
+        <div className='header'>
+            <div className='header__logo'>
+                <Link to='/'>Cargo Planner</Link>                
+            </div>
+            <div className='header__search'>
+                <input
+                    //value={ searchText ? searchText : '' }
+                    onChange={(e) => onFilterCompany(e.target.value)}
+                    placeholder='Search' />
+            </div>
+            <div className='header__actions'>
+                <button
+                    onClick={onFathCompanies}
+                >Load</button>
+                <button>Save</button>
+            </div>
+        </div>
     )
 }
 
-export default Header;
+const mapStateToProps = ({ companyList: { searchText } }) => {
+    return {searchText}
+}
+
+const mapDispatchToProps = (dispatch, { companyService }) => {
+    return {
+        onFilterCompany: (text) => dispatch(filterCompanyByName(text)),
+        onFathCompanies: fetchCompaniesFromGitHub(companyService, dispatch,),
+    }
+}
+
+export default withCompanyService()(connect(mapStateToProps, mapDispatchToProps)(Header));
